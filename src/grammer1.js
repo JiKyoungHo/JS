3db32e45c760d9548bcd6ebc2b2f36feecd74112
JiKyoungHo <36524129+JiKyoungHo@users.noreply.github.com>
2020-03-06 16:31:40 +0900

    grammer1

---
+++ src/grammer1.js
@@ -0,0 +1,9 @@
+let string = [];
+if (string.length === 0) {
+  console.log(`${string.length} : 비어있는 배열이다`);
+} else {
+  console.log(`${string.length} : 비어 있지 않음.`);
+}
+
+let text = string === 0 ? "비어있다" : "비어있지 않음";
+console.log(text);
